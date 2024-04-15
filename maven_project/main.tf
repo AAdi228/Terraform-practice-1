42e843f9a98c84d97f18f10d7e082e85d65ed72f
AAdi228 <162331634+AAdi228@users.noreply.github.com>
2024-04-15 17:04:27 +0530

    Update main.tf

--- maven_project/main.tf
+++ maven_project/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "Adithya" {
   ami = "ami-09298640a92b2d12c"
   instance_type = "t2.medium"
-  key_name = "chary"
+  key_name = "ggt"
   user_data              = file("userdata.sh") 
   tags = {
-    Name = "Adithya"
+    Name = "cherry"
   }
 }
