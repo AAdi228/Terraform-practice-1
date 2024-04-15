0497dc9ecb65d04e85d31d8a271e3d22f85ba44d
Adithya <cherrylazy45@gmail.com>
2024-04-15 17:01:02 +0530

    updates

---
+++ maven_project/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "Adithya" {
+  ami = "ami-09298640a92b2d12c"
+  instance_type = "t2.medium"
+  key_name = "chary"
+  user_data              = file("userdata.sh") 
+  tags = {
+    Name = "Adithya"
+  }
+}
